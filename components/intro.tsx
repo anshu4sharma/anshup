ae0f97378005994ba4fcd60be6d8cb27fc739c8b
Anshu Sharma <anshusharma6327@gmail.com>
2023-10-14 21:34:00 +0530

    personal profile removed

--- components/intro.tsx
+++ components/intro.tsx
@@ -100,7 +100,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/anshu24/"
+          href="https://www.linkedin.com/"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +108,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/anshu0x"
+          href="https://github.com"
           target="_blank"
         >
           <FaGithubSquare />
